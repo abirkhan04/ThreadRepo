c29b48dffd8a106066c005ecdf72a2a387807413
Abir Khan <abir1sumon@gmail.com>
2016-04-15 17:14:38 +0600

    Initial checking of thread project

---
+++ ThreadTest/src/CountX.java
@@ -0,0 +1,28 @@
+public class CountX extends Thread {
+	private static int X=0;	
+		
+	public CountX() {
+		// TODO Auto-generated constructor stub			
+				
+	}
+	
+	@Override
+	public void run() {
+		int i=0;
+		// TODO Auto-generated method stub
+		for( i=0;i<10000;i++){
+			 X = X+1; 
+		}
+	}
+	
+	public static void main(String args[]){
+		  Thread test[] = new CountX[5];
+		  for(int i=0;i<test.length;i++){
+			  test[i] = new CountX();
+			  test[i].start();
+			  System.out.println(X);
+			  
+		  }
+		  System.out.println("final"+X);
+	}
+}
